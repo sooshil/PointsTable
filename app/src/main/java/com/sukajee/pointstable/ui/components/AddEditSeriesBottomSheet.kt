9e3e728126701a28761ac9fd0a0b6e0fd7172425
sooshil <sushilkaf@gmail.com>
2023-12-01 23:02:17 -0500

    bottomsheet data entry validation

--- app/src/main/java/com/sukajee/pointstable/ui/components/AddEditSeriesBottomSheet.kt
+++ app/src/main/java/com/sukajee/pointstable/ui/components/AddEditSeriesBottomSheet.kt
@@ -13,10 +13,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -26,8 +30,6 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -36,6 +38,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sukajee.pointstable.data.model.Series
@@ -71,6 +74,8 @@ fun AddEditSeriesBottomSheet(
             mutableStateOf(emptyList())
         }
     }
+    var isErrorInSeriesName by rememberSaveable { mutableStateOf(false) }
+    var insufficientTeams by rememberSaveable { mutableStateOf(false) }
 
     ModalBottomSheet(
         sheetState = sheetState,
@@ -108,15 +113,34 @@ fun AddEditSeriesBottomSheet(
                         value = seriesName,
                         onValueChange = {
                             seriesName = it
+                            isErrorInSeriesName = false
                         },
                         maxLines = 1,
                         label = {
                             Text(text = "Series Name")
+                        },
+                        isError = isErrorInSeriesName,
+                        supportingText = {
+                            if (isErrorInSeriesName) {
+                                Text(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = "Series name cannot be empty",
+                                    color = MaterialTheme.colorScheme.error
+                                )
+                            }
+                        },
+                        trailingIcon = {
+                            if (isErrorInSeriesName)
+                                Icon(
+                                    Icons.Filled.Warning,
+                                    "error",
+                                    tint = MaterialTheme.colorScheme.error
+                                )
                         }
                     )
                 }
                 item {
-                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.height(4add.dp))
                     OutlinedTextField(
                         modifier = Modifier
                             .fillMaxWidth(),
@@ -151,19 +175,37 @@ fun AddEditSeriesBottomSheet(
                         }
                     )
                 }
+                if (insufficientTeams) {
+                    item {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            text = "At least two teams are required to proceed. Add teams",
+                            color = MaterialTheme.colorScheme.error,
+                            fontSize = 12.sp
+                        )
+                    }
+                }
                 item {
-                    Spacer(modifier = Modifier.height(4.dp))
+                    Spacer(modifier = Modifier.height(8.dp))
                     TextButton(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .align(Alignment.End),
+                            .fillMaxWidth(),
                         onClick = {
+                            insufficientTeams = false
                             val existingTeams = teams.toMutableList()
                             existingTeams.add("")
                             teams = existingTeams.toList()
                         },
                     ) {
-                        Text(text = "Add Team")
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Add Team",
+                            fontSize = 14.sp,
+                            textAlign = TextAlign.Start
+                        )
                     }
                 }
                 item {
@@ -186,6 +228,14 @@ fun AddEditSeriesBottomSheet(
                         Button(
                             modifier = Modifier.weight(1f),
                             onClick = {
+                                if (seriesName.isEmpty()) {
+                                    isErrorInSeriesName = true
+                                    return@Button
+                                }
+                                if (teams.filter { it.isNotEmpty() }.size < 2) {
+                                    insufficientTeams = true
+                                    return@Button
+                                }
                                 val seriesToStore = if (inEditMode && series != null) {
                                     series.copy(
                                         name = seriesName,
